8374a83df1d122be07a65ae3f33d9b1b46de897e
itgoze211 <44012889+itgoze211@users.noreply.github.com>
2018-11-02 16:11:00 -0500

    Initial Solution Setup

---
+++ ShopProj/ShopProj.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopProj.WebUI.Startup))]
+namespace ShopProj.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
